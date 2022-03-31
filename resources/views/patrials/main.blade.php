57886e699e670477cc1efa29dd73481803fa1b3c
Patricia Ho <hopatricia1711@gmail.com>
2022-03-31 12:41:16 +0700

    add link for css and js

--- resources/views/patrials/main.blade.php
+++ resources/views/patrials/main.blade.php
@@ -13,6 +13,18 @@
             crossorigin="anonymous"
         />
 
+        {{-- CSS --}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+        <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+
+        <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+        <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+        <link rel="stylesheet" href="assets/css/app.css">
+        <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+
         <title>{{ $title }}</title>
     </head>
 <body>
@@ -21,18 +33,24 @@
     <div class="container-fluid">
         <div class="row">
             @include('patrials.sidebar')
-      
+
           <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             @yield('container')
           </main>
         </div>
       </div>
-    
-    <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-></script>
+    crossorigin="anonymous"></script>
+
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+
+    <script src="assets/js/main.js"></script>
 
 <!-- Option 2: Separate Popper and Bootstrap JS -->
 <!--
