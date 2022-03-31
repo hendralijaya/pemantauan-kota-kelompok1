f0d028c069a45a218e60d079101e5e3c67527dcc
Hael12122002 <hendralijaya@gmail.com>
2022-03-31 11:39:43 +0700

    Initialized the project

---
+++ app/Http/Controllers/DashboardWebsiteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardWebsiteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view();
+    }
+    
+    public function bps2016()
+    {
+        return view();
+    }
+
+    public function bps2017()
+    {
+        return view();
+    }
+
+    public function bps2018()
+    {
+        return view();
+    }
+
+    public function bps2019()
+    {
+        return view();
+    }
+
+    public function bps2020()
+    {
+        return view();
+    }
+}
