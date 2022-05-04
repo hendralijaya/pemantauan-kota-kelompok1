100076f5b298c3383ad7e2366b27d43c6ed1cd0f
Patricia Ho <hopatricia1711@gmail.com>
2022-05-04 12:05:52 +0700

    kesimpulan aja

--- resources/views/dashboard/kesimpulan.blade.php
+++ resources/views/dashboard/kesimpulan.blade.php
@@ -176,7 +176,38 @@
                 <h4 class="section-title">Kepadatan Penduduk di Jawa Barat</h4>
             </div>
 
-            <div class="col-6">
+            <div class="col-12" style="margin-bottom:20px;">
+                <div class='tableauPlaceholder' id='viz1651638871792' style='position: relative; border-radius: 10px;'><noscript><a href='#'><img
+                    alt='Tingkat Kepadatan Penduduk di Jawa Barat '
+                    src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;te&#47;testone_16516386300470&#47;Sheet1&#47;1_rss.png'
+                    style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                <param name='embed_code_version' value='3' />
+                <param name='site_root' value='' />
+                <param name='name' value='testone_16516386300470&#47;Sheet1' />
+                <param name='tabs' value='no' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image'
+                    value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;te&#47;testone_16516386300470&#47;Sheet1&#47;1.png' />
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='en-US' />
+                <param name='filter' value='publish=yes' /></object></div>
+                <script type='text/javascript'>
+                    var divElement = document.getElementById('viz1651638871792');
+                    var vizElement = divElement.getElementsByTagName('object')[0];
+                    vizElement.style.width = '100%';
+                    vizElement.style.height = (divElement.offsetWidth * 0.625) + 'px';
+                    var scriptElement = document.createElement('script');
+                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+                    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+                </script>
+            </div>
+
+            <div class="col-6" style="margin-top: 20px; margin-bottom:20px;">
                 <div class='tableauPlaceholder' id='viz1651205732469' style='position: relative; border-radius: 10px;'>
                     <noscript><a href='#'><img alt='kepadatan penduduk '
                                 src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ke&#47;kepadatanpenduduk_16505328031940&#47;Sheet1&#47;1_rss.png'
@@ -208,8 +239,34 @@
                 </script>
             </div>
 
-            <div class="col-6">
-                <div class='tableauPlaceholder' id='viz1651211011388' style='position: relative'><noscript><a href='#'><img
+            <div class="col-6" style="margin-top: 20px; margin-bottom:20px;">
+                <h6>Q : Bagaimana kepadatan penduduk kabupaten - kabupaten yang ada di Jawa Barat dari tahun 2016-2020?</h6>
+                <p style="text-align:justify"><b>A :</b> Dari data kepadatan penduduk kabupaten/kota di Jawa Barat, selama 10 tahun terakhir terdapat 5 kota/kabupaten
+                    yang secara konsisten memiliki kepadatan penduduk yang paling tinggi yaitu, Kota Bogor, Kota Depok, Kota Bekasi,
+                    Kota Bandung dan Kota Cimahi dan 5 kota/kabupaten yang memiliki kepadatan penduduk yang paling rendah yaitu,
+                    Kabupaten Pengandaran, Kabupaten Cianjur, Kabupaten Sukabumi, Kabupaten Tasikmalaya, dan Kabupaten Sumedang.
+                </p>
+            </div>
+
+            <div class="col-6" style="margin-top: 20px; margin-bottom:20px;">
+                <h6>Q : Bagaimana estimasi kepadatan penduduk yang ada di Jawa Barat dari kini hingga tahun 2045? </h6>
+                <p style="text-align:justify"><b>A :</b> Berdasarkan data kepadatan penduduk yang didapatkan mulai dari tahun 2010-2020,
+                    hasil menunjukkan bahwa selalu ada peningkatan kepadatan penduduk secara konstan. Dari data tersebut, diperkirakan
+                    hingga tahun 2045 kepadatan penduduk akan meningkat sekitar 1.133.267 penduduk setiap tahun. Berdasarkan hasil
+                    estimasi prediksi sekitar 25 tahun dimulai dari tahun 2021 hingga 2045, total kenaikkan penduduk mencapai
+                    19.610.567.</p>
+                <p style="text-align:justify"><b>Note :</b> Kami memilih untuk melakukan prediksi hingga tahun 2045 karena kami mendapati bahwa tahun 2045 akan menjadi momentum bersejarah bagi Indonesia. Tingkat
+                    kelahiran yang meningkat pesat pada tahun 2020 dan 2021 karena situasi pandemi ini menjadi hal yang perlu diberikan
+                    perhatian khusus. Bayi lahir pada tahun tersebut akan menjadi penduduk berusia produktif pada 2045 mendatang.
+                    Berdasarkan berita yang kami peroleh di indonesiabaik.id, Indonesia akan mendapatkan bonus demografi yaitu 70% dari
+                    jumlah penduduk Indonesia dalam usia produktif (15-64 tahun), sedangkan sisanya 30% merupakan penduduk yang tidak
+                    produktif (usia dibawah 14 tahun dan diatas 65 tahun) pada periode tahun 2020-2045.</p>
+                <p><a href="https://indonesiabaik.id/infografis/siapkah-kamu-jadi-generasi-emas-2045"><b>Link berita bisa dilihat disini</b></a></p>
+                <br>
+            </div>
+
+            <div class="col-6" style="margin-top: 20px; margin-bottom:20px;">
+                <div class='tableauPlaceholder' id='viz1651211011388' style='position: relative; border-radius:10px'><noscript><a href='#'><img
                                 alt='Kepadatan Penduduk di Jawa Barat  '
                                 src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ke&#47;KepadatanPendudukdiJawaBarat&#47;KepadatanPendudukdiJawaBarat&#47;1_rss.png'
                                 style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
@@ -233,21 +290,11 @@
                     var divElement = document.getElementById('viz1651211011388');
                     var vizElement = divElement.getElementsByTagName('object')[0];
                     vizElement.style.width = '100%';
-                    vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+                    vizElement.style.height = (divElement.offsetWidth * 0.875) + 'px';
                     var scriptElement = document.createElement('script');
                     scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
                     vizElement.parentNode.insertBefore(scriptElement, vizElement);
                 </script>
-            </div>
-
-            <div class="col-12 mt-3 mb-1">
-                <br>
-                <h6>Q : Bagaimana kepadatan penduduk kabupaten - kabupaten yang ada di Jawa Barat dari tahun 2016-2020?</h6>
-                <p>A : </p>
-                <br>
-                <h6>Q : Bagaimana estimasi kepadatan penduduk yang ada di Jawa Barat dari kini hingga tahun 2045? </h6>
-                <p>A : </p>
-                <br>
             </div>
             {{-- Analisis kepadatan Penduduk Ends --}}
 
